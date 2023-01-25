fe90d449328ac116a37575425132ef8efdf20d03
Nickon Babaei <babaein1@gmail.com>
2023-01-25 17:51:49 -0300

    made dice roll show in ascending order

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 let blueChips = 20
 let redChips = 20
 let playerTurn = 'Blue'
-const blueRoll = []
-const redRoll = []
+let rolled = []
 const dice1 = document.querySelector('.dice-1')
 const dice2 = document.querySelector('.dice-2')
 const dice3 = document.querySelector('.dice-3')
 const toRoll = document.querySelector('.roll')
 
 const roll = () => {
-    dice1.innerHTML = Math.floor((Math.random() * 6)+1)
-    dice2.innerHTML = Math.floor((Math.random() * 6)+1)
-    dice3.innerHTML = Math.floor((Math.random() * 6)+1)
     
+    diceRoll1 = Math.floor((Math.random() * 6)+1)
+    diceRoll2 = Math.floor((Math.random() * 6)+1)
+    diceRoll3 = Math.floor((Math.random() * 6)+1)
+    rolled = [diceRoll1, diceRoll2, diceRoll3]
+    rolled.sort((a,b)=>a-b)
+    dice1.innerHTML = rolled[0]
+    dice2.innerHTML = rolled[1]
+    dice3.innerHTML = rolled[2]
 }
  
 
