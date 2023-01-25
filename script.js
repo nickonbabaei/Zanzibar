08464d76ed1a75710c0375a8f868bb92cbf81173
Nickon Babaei <babaein1@gmail.com>
2023-01-25 16:33:35 -0300

    generated dice roll

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let blueChips = 20
 let redChips = 20
+let playerTurn = 'Blue'
 const blueRoll = []
 const redRoll = []
-let dice1
-let dice2
-let dice3
+const dice1 = document.querySelector('.dice-1')
+const dice2 = document.querySelector('.dice-2')
+const dice3 = document.querySelector('.dice-3')
 const toRoll = document.querySelector('.roll')
+
+const roll = () => {
+    dice1.innerHTML = Math.floor((Math.random() * 6)+1)
+    dice2.innerHTML = Math.floor((Math.random() * 6)+1)
+    dice3.innerHTML = Math.floor((Math.random() * 6)+1)
+    
+}
+ 
+
+
+
+toRoll.addEventListener('click', () => {
+    roll()
+})
